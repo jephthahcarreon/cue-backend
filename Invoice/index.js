d615a107ad066f75abc487c4b72eb9f5ca64efd3
Jephthah Carreon <jephthah.carreon@gmail.com>
2025-03-19 04:13:58 +0800

    add security middlewares

--- Invoice/index.js
+++ Invoice/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const rateLimit = require('express-rate-limit');
+const helmet = require("helmet");
 const createHandler = require("azure-function-express").createHandler;
 const invoiceDAO = require("../common/database/dao/invoice-dao");
 const { validateApiKey } = require("../common/middleware");
 const utility = require("../common/utility");
 const app = express();
-
 const INVOICE_API_KEY = process.env.INVOICE_API_KEY
-app.use(validateApiKey(INVOICE_API_KEY));;
+
+app.use(rateLimit({
+    windowMs: 60 * 1000,
+    max: 10,
+    message: "Too many requests, please try again later.",
+    headers: true
+}));
+app.use(helmet());
+app.use(validateApiKey(INVOICE_API_KEY));
 
 app.post("/api/invoice/create", async(req, res) => {
     req.context.log('Create Invoce');
--- Order/index.js
+++ Order/index.js
@@ -1,14 +1,32 @@
 const express = require("express");
+const Redis = require("ioredis");
+const rateLimit = require('express-rate-limit');
+const helmet = require("helmet");
 const createHandler = require("azure-function-express").createHandler;
 const orderDAO = require("../common/database/dao/order-dao");
 const { validateApiKey } = require("../common/middleware");
 const utility = require("../common/utility");
 const app = express();
-
+const redis = new Redis();
 const ORDER_API_KEY = process.env.ORDER_API_KEY;
-app.use(validateApiKey(ORDER_API_KEY));;
+
+app.use(rateLimit({
+    windowMs: 60 * 1000,
+    max: 10,
+    message: "Too many requests, please try again later.",
+    headers: true
+}));
+app.use(helmet());
+app.use(validateApiKey(ORDER_API_KEY));
 
 app.get("/api/order/list", async(req, res) => {
+    const cacheKey = "orders";
+    const cachedData = await redis.get(cacheKey);
+    if (cachedData) {
+        req.context.log("Returning cached Orders data from Redis.");
+        return res.status(200).json(JSON.parse(cachedData));
+    }
+
     const status = req.query.status;
     const startDate = req.query.startDate;
     const endDate = req.query.endDate;
@@ -16,6 +34,8 @@ app.get("/api/order/list", async(req, res) => {
     try {
         await utility.validator(require("../Order/get-order-list.schema.json"), req.body, req.context);
         const orders = await orderDAO.getOrderList(status, startDate, endDate, req.body.pageNumber, req.body.pageSize, req.context);
+        req.context.log("Caching Orders data to Redis.");
+        await redis.set(cacheKey, JSON.stringify(orders), "EX", 300);
         res.status(200).json(orders);
     } catch (err) {
         utility.handleErrorResponse(err, res, req.context);
@@ -38,6 +58,8 @@ app.post("/api/order/create", async(req, res) => {
     try {
         await utility.validator(require("../Order/create-order.schema.json"), req.body, req.context);
         const orderSummary = await orderDAO.createOrder(req.body.customerId, req.body.userId, req.body.items, req.context);
+        req.context.log("Clearing cached Orders data from Redis.");
+        await redis.del("orders");
         res.status(200).json(orderSummary);
     } catch (err) {
         utility.handleErrorResponse(err, res, req.context);
